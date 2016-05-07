741f06b57559f9a1a9c14e732dfb1e88b0c17f3c
Ian Simpson <IDFSimpson@ymail.com>
2016-05-07 10:36:08 -0700

    add connections between organization and users

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,5 +2,10 @@ class Organization < ActiveRecord::Base
   has_many :tech_taggings, dependent: :destroy
   has_many :tech_stacks, through: :tech_taggings
 
+  belongs_to :user
+
+  has_many :pending_requests
+  has_many :pending_mangers, through: :pending_requests, source: :user
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  belongs_to :organization
+
+  has_many :pending_requests
+  has_many :pending_organization, through: :pending_requests, source: :organization
 end
