fd7cf9556b7bfd477a8851d9cae6c4899776e0dc
Ian Simpson <IDFSimpson@ymail.com>
2016-05-07 10:35:27 -0700

    add connections between organization and tech stack models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,6 @@
 class Organization < ActiveRecord::Base
+  has_many :tech_taggings, dependent: :destroy
+  has_many :tech_stacks, through: :tech_taggings
+
+
 end
--- app/models/tech_stack.rb
+++ app/models/tech_stack.rb
@@ -1,2 +1,4 @@
 class TechStack < ActiveRecord::Base
+  has_many :tech_taggings, dependent: :destroy
+  has_many :tagged_organizations, through: :tech_taggings, source: :organization
 end
