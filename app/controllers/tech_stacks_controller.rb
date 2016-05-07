1a48f0ac71aba0abbc733aa0c3b184311f813f50
Ian Simpson <IDFSimpson@ymail.com>
2016-05-07 11:07:10 -0700

    add a controller for tech stacks

---
+++ app/controllers/tech_stacks_controller.rb
@@ -0,0 +1,32 @@
+class TechStacksController < ApplicationController
+  def index
+    @tech_stacks = TechStack.all
+    respond_to do |format|
+      # format.html { render }
+      format.json { render json: @tech_stacks.to_json }
+    end
+  end
+
+  def create
+    tech_stack_params = params.require(:tech_stack).permit(:title, :description, :price)
+    @tech_stack = TechStack.new(tech_stack_params)
+
+    if @tech_stack.save
+      redirect_to tech_stack_path(@tech_stack)
+    # else
+    #   render :new
+    end
+  end
+
+  def show
+    @tech_stack = TechStack.find params[:id]
+    respond_to do |format|
+      # format.html { render :show }
+      # format.js {render json: @tech_stack }
+      # format.js { render js: "alert('Can\'t work, please refresh the page!');" }
+      format.json { render json: @tech_stack.to_json }
+    end
+    # render :show
+  end
+
+end
