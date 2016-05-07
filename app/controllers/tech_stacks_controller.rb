5b2416944064e5ce06769a90fcdd567e378a9a77
Quan Lee <qlee50@icloud.com>
2016-05-07 16:26:52 -0700

    create, delete tech stack completed

--- app/controllers/tech_stacks_controller.rb
+++ app/controllers/tech_stacks_controller.rb
@@ -1,26 +1,28 @@
 class TechStacksController < ApplicationController
-  def index
-    @tech_stacks = TechStack.all
-    respond_to do |format|
-      format.json { render json: @tech_stacks.to_json }
-    end
-  end
 
   def create
-    tech_stack_params = params.require(:tech_stack).permit(:title, :description, :price)
+    tech_stack_params = params.require(:tech_stack).permit(:name)
     @tech_stack = TechStack.new(tech_stack_params)
 
-    if @tech_stack.save
-      redirect_to tech_stack_path(@tech_stack)
-    # else
-    #   render :new
+    respond_to do |format|
+      if @tech_stack.save
+        format.js { render :create_success }
+      else
+        format.js { render :create_failure }
+      end
     end
   end
 
-  def show
+  def new
+    @tech_stack = TechStack.new
+    render :new
+  end
+
+  def destroy
     @tech_stack = TechStack.find params[:id]
+    @tech_stack.destroy
     respond_to do |format|
-      format.json { render json: @tech_stack.to_json }
+      format.js { render :delete_success }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :tech_stacks, only: [:show, :index]
+  resources :tech_stacks, only: [:create, :new, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
