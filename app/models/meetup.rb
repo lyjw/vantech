8b90432c9e0605980d57d7f62d9c5b768293a719
Jane Wong <jaynellew@gmail.com>
2016-05-08 11:17:35 -0700

    AJAXify add and delete meetup, update seed events method, add meetup validations, update admin nav links

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,4 +1,8 @@
 class Meetup < ActiveRecord::Base
+  has_many :events, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: { message: "This Meetup is already in your list." }
+
   def total_events
     conn = Faraday.new(url: 'https://api.meetup.com') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
@@ -6,13 +10,19 @@ class Meetup < ActiveRecord::Base
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
 
-    response = conn.get url     # GET http://sushi.com/nigiri/sake.json
+    response = conn.get url
     JSON.parse(response.body)
   end
 
   def seed_events
+    two_months_later = Date.today + 2.months
+
     total_events.each do |event|
-      Event.create(title: event["name"], url: event["link"], description: event["description"], group_name: event["group"]["name"], time: Time.at(event["time"]/1000).localtime, meetup_id: id)
+      event_time = event["time"]/1000
+
+      if Time.at(event_time) < two_months_later
+        Event.create(title: event["name"], url: event["link"], description: event["description"], group_name: event["group"]["name"], time: Time.at(event_time).localtime, meetup_id: id)
+      end
     end
   end
 end
