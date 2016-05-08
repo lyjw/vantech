54248d5d995ee65413bbf9c4ff673d0cf6765f4c
Jane Wong <jaynellew@gmail.com>
2016-05-08 13:40:51 -0700

    Create Update Events job and rake task

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,26 +6,4 @@ class EventsController < ApplicationController # :nodoc:
   def show
     # show
   end
-
-  private
-
-  # Method to gather events parsed with JSON via API link
-  def total_events
-    url = "/HackerNestVan/events?photo-host=public&page=20&sig_id=204710864&sig=bd79ac9a7272dfe2a5a40a8a956b27ea0e1fdb85"
-
-    conn = Faraday.new(url: 'https://api.meetup.com') do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-
-    response = conn.get url     # GET http://sushi.com/nigiri/sake.json
-    JSON.parse(response.body)
-  end
-
-  def create_event_objects
-    total_events.each do |event|
-      Event.create(title: event["name"], url: event["link"], description: event["description"], group_name: event["group"]["name"], time: Time.at(event["time"]/1000))
-    end
-  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base # :nodoc:
   belongs_to :meetup
 
+  validates :title, uniqueness: { scope: :time }
+
   def start_time
     self.time
   end
