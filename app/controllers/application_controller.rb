6ccbd825a90bc68bdc54562a7065c5d329173700
Jonathan <jon.wong92@gmail.com>
2016-05-08 20:26:13 -0700

    Button added for filter but not functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,12 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_admin!
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    puts "Current user: #{current_user}"
-    puts "Admin: #{current_user.admin}"
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     redirect_to root_path, notice: "You do not have the access rights to that page" unless current_user.admin
   end
+
+  # all_tech_stacks method create to access tech stacks in home controller
+  def all_tech_stacks
+    @techstacks = TechStack.all
+  end
+  helper_method :all_tech_stacks
 end
